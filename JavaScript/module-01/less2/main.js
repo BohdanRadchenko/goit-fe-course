a8737b839ef4244f3e4e29b3066753713ce46971
BohdanRadchenko <radchenkobs@gmail.com>
2019-05-28 12:24:44 +0300

    add less 2 of  module 1

---
+++ JavaScript/module-01/less2/main.js
@@ -0,0 +1,35 @@
+'use strickt';
+
+// const hesNumber = Number(prompt('Enter number'));
+// if (hesNumber > 30 && hesNumber < 100 ) {
+//     console.log('hesNumber true :', hesNumber);
+// } else {
+//     console.log('hesNumber false :', hesNumber);
+// }
+
+//-------------- ternat ----------
+
+// const name = 'microsoft';
+// const showName = name === 'microsoft' ? console.log(true) : console.log(false); 
+
+// const urName = prompt('Ur name');
+// const name = 'show' 
+// const showName = name === urName ? console.log(true) : console.log(false); 
+
+
+//----------switch-------
+
+// const name = prompt('enter name ');
+// let result;
+// switch(name) {
+//     case 'Toha':
+//     result = 'Hello Toha';
+//     break;
+//     case 'Bond':
+//     result = 'Hello Bond';
+//     break;
+//     default:
+//     result = 'Who are u?';
+// }
+
+// console.log('result :', result);
