15c89ae114575f7912421150a77c573f6117bae6
BohdanRadchenko <radchenkobs@gmail.com>
2019-06-03 16:23:33 +0300

    add module 3

---
+++ JavaScript/module-03/dop/test_04/main.js
@@ -0,0 +1,32 @@
+'use strict';
+/*  
+  Написать функцию, getPx(str) 
+
+  Функция getPx должна получать строку вида '10px',
+  проверять была ли передана строка, если да, 
+  возвращать только числовую составляющую, к примеру 10.
+    
+  Если была передана не строка, функция возвращает null.
+*/
+
+function getPx(str) {
+  console.log('parseFloat(str) :', parseFloat(str));
+  if (typeof str === 'string') {
+    return Number.parseFloat(str);
+  }
+  return null;
+  
+  // if(Number.isNaN(Number(str))) {
+  //   return Number.parseFloat(str);
+  // }
+  // return null
+}
+
+// Вызовы функции для проверки
+console.log( getPx("10px") === 10 ); // true
+console.log( getPx("10.5") === 10.5 ); // true
+console.log( getPx("0") === 0 ); // true
+console.log( getPx(-1) ); // null
+console.log( getPx(10) ); // null
+
+
